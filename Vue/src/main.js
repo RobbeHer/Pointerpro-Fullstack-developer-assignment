4a5e4f05bb6f0b834a233826e4c045eff6d674bd
RobbeHer <robbe.heremans.r@gmail.com>
2023-01-20 18:31:05 +0100

    pinia-plugin-persistedstate

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
@@ -7,6 +8,7 @@ import './axios'
 
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 app.use(router)
 app.use(pinia)
